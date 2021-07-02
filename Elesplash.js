2517be70b49003c767a7946cfa51eea817965602
bo327 <80336157+bo327@users.noreply.github.com>
2021-07-02 13:48:05 +0530

    Add files via upload

---
+++ Elesplash.js
@@ -0,0 +1,19 @@
+//THIS IS A GAME WHERE YOU LEAD A ELEPHANT OUT OF A ZOO, AS IT FACES A LOT OF OBSTACLES
+var elephant,animal
+var health,bar,score;
+var stone,sand,trap,trap2;
+function preload ()
+{
+ 
+}
+
+function setup ()
+{
+ var elephant = createSprite(200,200,10,10);
+}
+
+function draw ()
+{
+  canvas(1500,1500);
+  display(elephant);
+}
